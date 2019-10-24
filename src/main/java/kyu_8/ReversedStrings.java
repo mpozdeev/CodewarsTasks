d1502970dedbb4e3bffdf0134d5a09bc45563fd5
Maksim <maximglazov18@gmail.com>
2019-10-24 23:50:48 +0400

    Add kata: Opposite Number

--- src/main/java/kyu_8/ReversedStrings.java
+++ src/main/java/kyu_8/ReversedStrings.java
@@ -3,11 +3,12 @@ package kyu_8;
 /**
  * Instructions:
  * Complete the solution so that it reverses the string value passed into it.
- * 
+ *
  * Example Kata.solution("world") //returns "dlrow"
  */
 
 public class ReversedStrings {
+
     public static String solution(String str) {
         StringBuilder sb = new StringBuilder(str);
         sb.reverse();
---
+++ src/test/java/kyu_8/OppositeNumberTest.java
@@ -0,0 +1,14 @@
+package kyu_8;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OppositeNumberTest {
+
+    @Test
+    public void opposite_Test(){
+        assertEquals(-5,OppositeNumber.opposite(5));
+    }
+
+}
